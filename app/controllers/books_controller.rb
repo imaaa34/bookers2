b005e1e3b3b7000cda0a124ed41830f2146213f9
imaaa34 <oct.imd34@gmail.com>
2021-04-25 06:51:15 +0000

    [Add] indexページのレイアウト追加、booksコントローラにcreateアクション追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,15 +4,30 @@ class BooksController < ApplicationController
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def index
+    #user-info
+    @user = User.find(current_user.id)
+    #new-book
+    @book = Book.new
+    #books
+    @books = Book.all
   end
 
   def show
   end
 
   def destroy
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
+  get 'homes/about' => 'homes#about'
   resources :books, only: [:new, :create, :index, :show, :destroy]
   resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
