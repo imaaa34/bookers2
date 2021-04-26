f91a54d1251574c5f657e4576a77d14ba40c79ca
imaaa34 <oct.imd34@gmail.com>
2021-04-26 03:57:37 +0000

    [Add] 部分テンプレート作成(変数定義は要修正)、user/indexページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
   def show
   end
 
+  def index
+    @user = User.find(current_user.id)
+    @users = User.all
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'homes/about' => 'homes#about'
   resources :books, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show]
+  resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
