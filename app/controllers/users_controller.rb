7db03c9ebc595c08e43285e71ed756a04950e11b
imaaa34 <oct.imd34@gmail.com>
2021-05-03 07:04:57 +0000

    [Update] 不要なルーティングとアクション削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,18 +9,6 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def create
-    #new-book
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    if @book.save
-      flash[:notice] = "You have created book successfully."
-      redirect_to book_path(@book.id)
-    else
-      render "books/index"
-    end
-  end
-
   def edit
     @user = User.find(params[:id])
     unless @user.id == current_user.id
@@ -48,16 +36,10 @@ class UsersController < ApplicationController
     end
   end
 
-
-
   private
 
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
-  end
-
-  def book_params
-    params.require(:book).permit(:title, :body)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions'}
   get 'home/about' => 'homes#about'
   resources :books, except: [:new]
-  resources :users, only: [:index, :create, :edit, :show, :update]
-  post 'users/:id' => 'users#create'
-  post 'books/:id' => 'books#create'
+  resources :users, only: [:index, :edit, :show, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
